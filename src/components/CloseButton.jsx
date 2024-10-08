a0a3a28e56350ca0641803c7e49218d6a6e07716
이해연 <gosl664@naver.com>
2024-10-08 18:22:41 +0900

    CloseButton 컴포넌트 작업 수정 완료 (추후 수정 가능성 있음)

---
+++ src/components/CloseButton.jsx
@@ -0,0 +1,22 @@
+// CloseButton.jsx
+import React from "react";
+import "./CloseButton.css";
+
+function CloseButton({ onClick, style }) {
+  return (
+    <button className="close-button" onClick={onClick} style={style}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        className="close-icon"
+      >
+        <line x1="4" y1="4" x2="20" y2="20" stroke="black" strokeWidth="2" />
+        <line x1="20" y1="4" x2="4" y2="20" stroke="black" strokeWidth="2" />
+      </svg>
+    </button>
+  );
+}
+
+export default CloseButton;
